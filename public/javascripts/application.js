45bf67148b76b60b66ff0e434024ee1f7de98c64
Scott Thorpe <sthorpe@gmail.com>
2009-11-09 18:15:40 -0800

    Fixed message view

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,6 @@
 function show_details(div,div2,div3,div4) {
 	e = document.getElementById(div4);
+  Effect.BlindDown(div, { duration: 0.1 });
   $(div3).style.display = 'inline';
   $(div2).style.display = 'none';
 	if (e.style.height.split('px')[0]<250) {e.style.height = 250 + 'px';
@@ -8,6 +9,7 @@ function show_details(div,div2,div3,div4) {
 
 function hide_details(div,div2,div3,div4) {
 	e = document.getElementById(div4);
+  Effect.BlindUp(div, { duration: 0.1 });
   $(div3).style.display = 'none';
   $(div2).style.display = 'inline';
 	if (e.style.height.split('px')[0]<250) {e.style.height = 250 + 'px';
