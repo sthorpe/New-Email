08579037d7884ef6b029ead864cc6092b2160774
Scott Thorpe <sthorpe@gmail.com>
2009-11-09 18:38:23 -0800

    Fixed message body and preview

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,19 @@
-function show_details(div,div2,div3,div4) {
+function show_details(div,div2,div3,div4,div5) {
 	e = document.getElementById(div4);
-  Effect.BlindDown(div, { duration: 0.1 });
+	$(div).style.display = 'inline';
   $(div3).style.display = 'inline';
   $(div2).style.display = 'none';
+	$(div5).style.display = 'none';
 	if (e.style.height.split('px')[0]<250) {e.style.height = 250 + 'px';
 	} else {e.style.height = 72 + 'px';}
 }
 
-function hide_details(div,div2,div3,div4) {
+function hide_details(div,div2,div3,div4,div5) {
 	e = document.getElementById(div4);
-  Effect.BlindUp(div, { duration: 0.1 });
+	$(div).style.display = 'none';
   $(div3).style.display = 'none';
   $(div2).style.display = 'inline';
+	$(div5).style.display = 'block';
 	if (e.style.height.split('px')[0]<250) {e.style.height = 250 + 'px';
 	} else {e.style.height = 72 + 'px';}
 
