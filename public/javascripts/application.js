8a82946880806c171d30ed075d22d17f08684c16
Scott Thorpe <sthorpe@gmail.com>
2009-11-09 18:09:38 -0800

    Fixed compose problem and cleaned up the history select

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,5 @@
 function show_details(div,div2,div3,div4) {
 	e = document.getElementById(div4);
-  Effect.BlindDown(div, { duration: 0.2 });
   $(div3).style.display = 'inline';
   $(div2).style.display = 'none';
 	if (e.style.height.split('px')[0]<250) {e.style.height = 250 + 'px';
@@ -9,7 +8,6 @@ function show_details(div,div2,div3,div4) {
 
 function hide_details(div,div2,div3,div4) {
 	e = document.getElementById(div4);
-  Effect.BlindUp(div, { duration: 0.2 });
   $(div3).style.display = 'none';
   $(div2).style.display = 'inline';
 	if (e.style.height.split('px')[0]<250) {e.style.height = 250 + 'px';
