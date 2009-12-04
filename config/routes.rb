6ff7a324a56edfe5d0ae3162ca8e0549cd00a08d
Scott Thorpe <sthorpe@gmail.com>
2009-12-04 14:48:44 -0800

    Fixed login/homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
 
-  map.root :controller => 'messages', :action => 'index'
+  map.root :controller => 'sessions', :action => 'new'
   
   map.resources :users
   
