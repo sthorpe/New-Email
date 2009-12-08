4caba621813a50c7283ec0a68b29bdb05bdea3d6
Scott Thorpe <sthorpe@gmail.com>
2009-12-07 18:35:25 -0800

    Fixed signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     if success && @user.errors.empty?
       @user.activate!
       redirect_back_or_default('/messages')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+      flash[:notice] = "Thanks for signing up!"
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
       render :action => 'new'
